8095e0535a5d62ab65287dfb612f96132721f2a2
meaggy <glowackm@mcmaster.ca>
2023-09-11 17:22:07 -0400

    Complete vertical stack of new reminders

--- public/main.js
+++ public/main.js
@@ -1,4 +1,47 @@
 
+// to do: 
+// build vertically
+// style input box
+// look at code, seperatation of concerns
+// add delete feature with garbage icon
+// add cross out feature
+// style background
+// fix div on click to create new
+// add button to create new item
+
+$(".creation-area").on("click", function(){
+    const prevText = $("#prev-reminder").val();  
+    const newInput = $("<input>").attr({"type":"text", "id":"prev-reminder", "class":"reminder-item"});
+    const newNote = $("<div>").append(newInput);
+    let initlized;
+    if (typeof prevText === "undefined" || prevText.trim() === "") {
+        $('#prev-reminder').focus();
+        initlized = true;         
+    }
+    else {
+        
+        const replaceInput = $("<input>").attr({"type":"text","class":"reminder-item"}).val(prevText);
+        const prevNote = $("<div>").append(replaceInput);
+        if (initlized === true){
+            $('.reminders-list').html(prevNote);
+            $('.reminders-list').append(newNote);
+            initlized = false;
+        }
+        else {
+            $("#prev-reminder").removeAttr("id");
+            $('.reminders-list').append(newNote);
+            $('#prev-reminder').focus();
+        };
+    };   
+}
+);
+
+    // const currentList = $(".reminders-list").html();
+    // const prevItem = $(".prev-reminder").val();
+    // const addNote = '<div><input type="text" id="prev-reminder" class="reminder-box"></input></div>';    
+    // $('.reminders-list').html(currentList+addNote);
+    // $('#current-reminder').focus();
+
 // Store var to be used in new creation....
 // $("#prev-reminder").change(function(){
 //     console.log("change triggered from obj type: input");
@@ -6,63 +49,4 @@
 //     alert( "\nNow: " + $this.val());
 // });
 
-
-$(".creation-area").on("click", function(){
-    const prevText = $("#prev-reminder").val();  
-    const addNote = $("<input>").attr({"type":"text", "id":"prev-reminder"});
-    let initlized;
-    //  can only create a new blank note when the previous one IS NOT empty
-    // previous one can be marked by the id... 
-      
-    if (typeof prevText === "undefined" || prevText.trim() === "") {
-        console.log("prev text IS null=>", prevText);
-        $('#prev-reminder').focus();
-        initlized = true;           
-        // if (initlized === false) {
-        //     console.log("trying to focus");
-        //     ('#prev-reminder').focus();
-        // };   
-        // console.log("not initalized");
-        // // $('.reminders-list').append(addNote); 
-        // $('#prev-reminder').focus();
-        // initlized = true;               
-    }
-    else {
-        console.log("prev text IS SOMETHING: =>", prevText);
-        const prevNote = $("<input>").attr({"type":"text", "id":"tester"}).val(prevText);
-
-        // const addNote = $("<input>").attr({"type":"text", "id":"prev-reminder"});
-        console.log(addNote);
-        if (initlized === true){
-            console.log("should overwtite HTML");
-            $('.reminders-list').html(prevNote);
-            $('.reminders-list').append(addNote);
-            initlized = false;
-        }
-        else {
-            console.log("append HTML");
-            $("#prev-reminder").removeAttr("id");
-            $('.reminders-list').append(addNote);
-            $('#prev-reminder').focus();
-        };
-    };   
-    // prevtext as soon as new note is added then, prev text should loose its id..... 
-    // TODO: ONE SHOULD REPLACE HTML THE OTHER SHOULD APPEND!
-    // need logic for this ^^^
-    
-    // const currentList = $(".reminders-list").html();
-    // const prevItem = $(".prev-reminder").val();
-    // const addNote = '<div><input type="text" id="prev-reminder" class="reminder-box"></input></div>';    
-    // $('.reminders-list').html(currentList+addNote);
-    // $('#current-reminder').focus();
-}
-);
-
-
-
-
-
-//TODO: persist data in text box... change element? replace? and use "this."
-//TODO: switch to using a button to create new reminders
-// must work on basic funcationality first
 
