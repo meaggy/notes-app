58866c27f353a1086f7d18e75e70304bb6b1e306
meaggy <glowackm@mcmaster.ca>
2023-09-10 21:55:03 -0400

    Fix bug in creating new empty inputs

--- public/main.js
+++ public/main.js
@@ -9,28 +9,44 @@
 
 $(".creation-area").on("click", function(){
     const prevText = $("#prev-reminder").val();  
+    const addNote = $("<input>").attr({"type":"text", "id":"prev-reminder"});
     let initlized;
-    console.log(prevText);  
+    //  can only create a new blank note when the previous one IS NOT empty
+    // previous one can be marked by the id... 
+      
     if (typeof prevText === "undefined" || prevText.trim() === "") {
-        // on first / initialization
         console.log("prev text IS null=>", prevText);
         $('#prev-reminder').focus();
-        initlized = true;
+        initlized = true;           
+        // if (initlized === false) {
+        //     console.log("trying to focus");
+        //     ('#prev-reminder').focus();
+        // };   
+        // console.log("not initalized");
+        // // $('.reminders-list').append(addNote); 
+        // $('#prev-reminder').focus();
+        // initlized = true;               
     }
     else {
         console.log("prev text IS SOMETHING: =>", prevText);
-        const prevNote = $("<input>").attr("type", "text").val(prevText);
-        const addNote = $("<input>").attr({"type":"text", "id":"prev-reminder"});
+        const prevNote = $("<input>").attr({"type":"text", "id":"tester"}).val(prevText);
+
+        // const addNote = $("<input>").attr({"type":"text", "id":"prev-reminder"});
         console.log(addNote);
         if (initlized === true){
+            console.log("should overwtite HTML");
             $('.reminders-list').html(prevNote);
             $('.reminders-list').append(addNote);
             initlized = false;
         }
         else {
+            console.log("append HTML");
+            $("#prev-reminder").removeAttr("id");
             $('.reminders-list').append(addNote);
+            $('#prev-reminder').focus();
         };
     };   
+    // prevtext as soon as new note is added then, prev text should loose its id..... 
     // TODO: ONE SHOULD REPLACE HTML THE OTHER SHOULD APPEND!
     // need logic for this ^^^
     
